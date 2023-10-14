51a00ff84be70a3f91e7e086c5ce70aa42c6df5e
rilala <toumingkuipingren@gmail.com>
2023-10-14 23:37:02 +0800

    编辑员工

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -26,6 +26,10 @@ public interface EmployeeService {
     PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
     void startOrStop(Integer status, Long id);
+
+    Employee getById(Long id);
+
+    void update(EmployeeDTO employeeDTO);
 }
 
 
